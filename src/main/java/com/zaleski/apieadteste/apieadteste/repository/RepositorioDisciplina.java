e95c2da63d85b33d0ede0f3ab8d6cd9e5fcb6ace
Thiago Zaleski Leon <thigofu@gmail.com>
2020-08-12 19:26:28 -0300

    Criação Crud Disciplina
    Realizado a criação dos métodos de CRUD de disciplina, conforme os outros

---
+++ src/main/java/com/zaleski/apieadteste/apieadteste/repository/RepositorioDisciplina.java
@@ -0,0 +1,9 @@
+package com.zaleski.apieadteste.apieadteste.repository;
+
+import com.zaleski.apieadteste.apieadteste.entity.Disciplina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioDisciplina extends JpaRepository<Disciplina,Integer> {
+    Disciplina findByName(String nome);
+    
+}
